e0a199bee426e95dd1c82106a01ccf2876d11485
atkhx <dr.drey.world@icloud.com>
2022-06-19 16:10:28 +0300

    cleanup

--- data/data.go
+++ data/data.go
@@ -9,7 +9,7 @@ type Data struct {
 	Data []float64
 }
 
-func (m *Data) Dimensions(dims ...*int) {
+func (m *Data) ExtractDimensions(dims ...*int) {
 	dimsCount := len(m.Dims)
 	for i, dim := range dims {
 		if i < dimsCount {
@@ -103,29 +103,6 @@ func (m Data) Copy() (r *Data) {
 	return
 }
 
-func (m *Data) RotateMatrixesInCube() {
-	w, h, d := m.Dims[0], m.Dims[1], m.Dims[2]
-
-	l := w * h
-
-	for z := 0; z < d; z++ {
-		m.Rotate(z*l, (z+1)*l)
-	}
-	return
-}
-
-func (m *Data) RotateMatrix(index int) {
-	l := m.Dims[0] * m.Dims[1]
-	m.Rotate(index*l, (index+1)*l)
-	return
-}
-
-func (m *Data) Rotate(from, to int) {
-	for i := 0; from+i < to-i-1; i++ {
-		m.Data[from+i], m.Data[to-i-1] = m.Data[to-i-1], m.Data[from+i]
-	}
-}
-
 func (m *Data) GetMinMaxValues(fromIndex, toIndex int) (min, max float64) {
 	min, max = m.Data[fromIndex], m.Data[fromIndex]
 	for i := fromIndex + 1; i < toIndex; i++ {
@@ -137,22 +114,4 @@ func (m *Data) GetMinMaxValues(fromIndex, toIndex int) (min, max float64) {
 		}
 	}
 	return
-}
-
-func (m *Data) GetMatrix(index int) *Data {
-	square := m.Dims[0] * m.Dims[1]
-	result := new(Data)
-	result.Dims = []int{m.Dims[0], m.Dims[1], 1}
-	result.Data = m.Data[index*square : (index+1)*square]
-
-	return result
-}
-
-func (m *Data) GetTensor(index, depth int) *Data {
-	cube := m.Dims[0] * m.Dims[1] * depth
-	result := new(Data)
-	result.Dims = []int{m.Dims[0], m.Dims[1], depth}
-	result.Data = m.Data[index*cube : (index+1)*cube]
-
-	return result
 }
