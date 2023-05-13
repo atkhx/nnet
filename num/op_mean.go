32542932d301c39076b82ff7bfd23307bbb15041
Andrey Tikhonov <dr.drey.world@icloud.com>
2023-05-13 12:23:36 +0300

    v2

--- num/op_mean.go
+++ num/op_mean.go
@@ -20,27 +20,3 @@ func (input *Data) Mean() *Data {
 
 	return output
 }
-
-func (input *Data) MeanByRows() *Data {
-	oDims := input.Dims
-	oDims.W = 1
-
-	output := New(oDims, input)
-
-	chunkSize := input.Dims.W
-	chunksCount := len(input.Data) / chunkSize
-
-	k := 1.0 / float64(chunkSize)
-
-	output.calcData = func() {
-		for i := 0; i < chunksCount; i++ {
-			output.Data[i] = input.Data[i*chunkSize:(i+1)*chunkSize].Sum() * k
-		}
-	}
-
-	output.calcGrad = func() {
-		input.Grad.AddWeighted(output.Grad, k)
-	}
-
-	return output
-}
